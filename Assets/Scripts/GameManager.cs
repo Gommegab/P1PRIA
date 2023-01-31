bcc6d8ad88aa1986088b36db4df3b578150a5d1c
Boromir <devirgommergab@gmail.com>
2023-01-31 13:15:55 +0100

    llamada a la api usando IEnumerator y leer un json y sacamos los datos por consola

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+
+        StartCoroutine(GetQuestions("https://opentdb.com/api.php?amount=10"));
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator GetQuestions(string uri)
     {
-        
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            yield return webRequest.SendWebRequest();
+            if (webRequest.result == UnityWebRequest.Result.Success)
+            {
+                string[] pages = uri.Split('/');
+                int page = pages.Length - 1;
+                LoadJson(webRequest.downloadHandler.text);
+
+            }
+            else
+            {
+                Debug.Log("No se puede conectar con la api");
+            }
+
+        }
+    }
+
+    public void LoadJson(string jsonString)
+    {
+        Questions q = JsonUtility.FromJson<Questions>(jsonString);
+        foreach (Question item in q.results)
+        {
+
+            Debug.Log($"Categoria : {item.category} | tipo : {item.type} | dificultad : {item.difficulty}");
+            Debug.Log($"Pregunta : {item.question}");
+            string txt = $" {item.correct_answer} ";
+            for (int i = 0; i < item.incorrect_answers.Length; i++)
+            {
+                txt += ($"| {item.incorrect_answers[i]} ");
+            }
+            Debug.Log(txt);
+            Debug.Log("__________________________________________________________________________________________________________________________________");
+        }
+
+
     }
 }
